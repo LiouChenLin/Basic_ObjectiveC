4a107922808d0a01cf4ea2784eab60f8e61d21af
AndyLiou <h1205542@gmail.com>
2018-03-17 18:36:55 +0800

    All of Exercises

--- CH2_Exercises/CH2_Exercises/main.m
+++ CH2_Exercises/CH2_Exercises/main.m
@@ -10,8 +10,25 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        ///Excercise 1
+        NSLog(@"In Objective-c, lowercase letters are significant.\nmain is where program excution begins.\nOpen and closed braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.");
+        
+        //Excercise 2
+        int i = 1;
+        NSLog(@"Testing...");
+        NSLog(@"....%i",i);
+        NSLog(@"...%i",i+1);
+        NSLog(@"..%i",i+2);
+        
+        //Result
+        /*
+         
+         Testing...
+         ....1
+         ...2
+         ..3
+         
+         */
     }
     return 0;
 }
