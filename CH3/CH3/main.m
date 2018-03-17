f5575a1e684ad60591ea566508c432b9aab999ea
AndyLiou <h1205542@gmail.com>
2018-03-17 22:19:02 +0800

    3-1

---
+++ CH3/CH3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CH3
+//
+//  Created by  AndyLiou on 2018/3/17.
+//  Copyright © 2018年 Andy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int numerator = 1;
+        int denominator = 3;
+        NSLog(@"The fraction is %i/%i", numerator , denominator);
+    }
+    return 0;
+}
