405f6e26d8f88ae7acac23190ea34b39a9ba8438
AndyLiou <h1205542@gmail.com>
2018-03-17 17:04:27 +0800

    Ch1

---
+++ CH2-1/CH2-1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CH2-1
+//
+//  Created by  AndyLiou on 2018/3/17.
+//  Copyright © 2018年 Andy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Programing is fun!");
+    }
+    return 0;
+}
