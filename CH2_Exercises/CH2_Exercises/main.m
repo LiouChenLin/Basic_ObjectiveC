6cbe18c5fa39545f6a010c62e19726a06c448682
AndyLiou <h1205542@gmail.com>
2018-03-17 18:44:00 +0800

    Add 4 to 6 exercises

--- CH2_Exercises/CH2_Exercises/main.m
+++ CH2_Exercises/CH2_Exercises/main.m
@@ -10,10 +10,10 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        ///Excercise 1
+        ///Excercise 2
         NSLog(@"In Objective-c, lowercase letters are significant.\nmain is where program excution begins.\nOpen and closed braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.");
         
-        //Excercise 2
+        //Excercise 3
         int i = 1;
         NSLog(@"Testing...");
         NSLog(@"....%i",i);
@@ -29,6 +29,29 @@ int main(int argc, const char * argv[]) {
          ..3
          
          */
+        
+        //Excercise 4
+        int count1 = 87;
+        int count2 = 15;  
+        NSLog(@"87 - 15 = %i",count1-count2);
+        
+        //Excercise 5
+        
+        int sum;//INT sum;  ----> int sum;
+        
+        sum = 25 + 37 -19;
+        
+        NSLog(@"The answer is %i",sum);
+        
+        //Excercise 6
+        int answer, result;
+        
+        answer = 100;
+        result = answer - 10;
+        
+        NSLog(@"The result is %i\n", result + 5);
+        
+        //Result ==> The result is 95
     }
     return 0;
 }
