d7ed831158406ce917de5304832af0c9bea2e890
AndyLiou <h1205542@gmail.com>
2018-03-17 17:30:04 +0800

    add excercises

---
+++ CH2_Exercises/CH2_Exercises/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CH2_Exercises
+//
+//  Created by  AndyLiou on 2018/3/17.
+//  Copyright © 2018年 Andy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
